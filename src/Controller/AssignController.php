9cf25f3a5602e480a24705ba56cf10704f042b0b
Darylabrador <abrador.daryl@gmail.com>
2020-11-30 04:41:28 +0400

    Creer une attribution

--- src/Controller/AssignController.php
+++ src/Controller/AssignController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Assign;
+use App\Repository\AssignRepository;
+use App\Repository\ClientRepository;
+use App\Repository\ComputerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class AssignController extends AbstractController
 {
     /**
-     * @Route("/assign", name="assign")
+     * @Route("/api/computer/attribution", name="attribution", methods={"POST"})
      */
-    public function index(): Response
+    public function index(Request $request, SerializerInterface $serializer, ClientRepository $clientRepository, ComputerRepository $computerRepository): JsonResponse
     {
-        return $this->render('assign/index.html.twig', [
-            'controller_name' => 'AssignController',
-        ]);
+        $data     = json_decode($request->getContent(), true);
+        $client   = $clientRepository->find($data['desktop_id']);
+        $computer = $computerRepository->find($data['client_id']);
+
+        $date = new \DateTime('@' . strtotime($data['date']));
+
+        $attribution = new Assign();
+        $attribution->setHours($data['hours']);
+        $attribution->setDate($date);
+        $attribution->setClient($client);
+        $attribution->setComputer($computer);
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($client);
+        $doctrine->persist($attribution);
+        $doctrine->flush();
+
+        $json = $serializer->serialize($attribution, 'json', ['groups' => 'attribution']);
+        $response = new JsonResponse($json, 200, [], true);
+        return $response;
     }
 }
